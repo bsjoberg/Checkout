a9d5a654c2b7eebbfcb9ec8b1eea81267c85b109
brian <brian@DESKTOP-F86LAQ0.fios-router.home>
2018-10-06 19:49:59 -0400

    Initial stubs of Checkout, CheckoutUnit, CheckoutSteps

---
+++ Checkout/src/test/java/com/checkout/Checkout.java
@@ -0,0 +1,14 @@
+package com.checkout;
+
+public class Checkout {
+
+	public Checkout() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void add(int itemCount, int bananaPrice) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
--- Checkout/src/test/java/com/checkout/CheckoutSteps.java
+++ Checkout/src/test/java/com/checkout/CheckoutSteps.java
@@ -6,15 +6,17 @@ import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
 public class CheckoutSteps {
+	private int bananaPrice = 0;
+	
 	@Given("^the price of a \"([^\"]*)\" is (\\d+)c$")
 	public void thePriceOfAIsC(String name, int price) throws Throwable {
-	    int bananaPrice = price;
+	    bananaPrice = price;
 	}
 
 	@When("^I checkout (\\d+) \"([^\"]*)\"$")
-	public void iCheckout(int arg1, String arg2) throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+	public void iCheckout(int itemCount, String itemName) throws Throwable {
+	    Checkout checkout = new Checkout();
+	    checkout.add(itemCount, bananaPrice);
 	}
 
 	@Then("^the total price should be (\\d+)c$")
