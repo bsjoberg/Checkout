8b54a385b2b754681f43ac88649fda47cd438ab7
brian <brian@DESKTOP-F86LAQ0.fios-router.home>
2018-10-06 20:06:25 -0400

    Initial add and subtract for checkout.

--- Checkout/src/test/java/com/checkout/Checkout.java
+++ Checkout/src/test/java/com/checkout/Checkout.java
@@ -1,14 +1,23 @@
 package com.checkout;
 
 public class Checkout {
-
-	public Checkout() {
+	private int itemCount = 0;
 	
+	public Checkout() {
+		
 	}
 
 	public void add(int itemCount, int bananaPrice) {
+		this.itemCount = this.itemCount + itemCount;
+	}
+
+	public int itemCount() {
 		// TODO Auto-generated method stub
-		
+		return itemCount;
+	}
+
+	public void subtract(int itemCount) {
+		this.itemCount = this.itemCount = itemCount;
 	}
 
 }
--- Checkout/src/test/java/com/checkout/CheckoutUnit.java
+++ Checkout/src/test/java/com/checkout/CheckoutUnit.java
@@ -7,9 +7,23 @@ import org.junit.Test;
 public class CheckoutUnit {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
-		
+	public void testAddOneItem() {
+		Checkout checkout = new Checkout();
+		checkout.add(1, 10);
+		assertEquals(1, checkout.itemCount());
 	}
 
+	@Test
+	public void testAddTwoItems() {
+		Checkout checkout = new Checkout();
+		checkout.add(2, 10);
+		assertEquals(2, checkout.itemCount());
+	}
+	
+	@Test
+	public void testAddTwoItemsSubstractOne() {
+		Checkout checkout = new Checkout();
+		checkout.add(2, 10);
+		checkout.subtract(1);
+	}
 }
